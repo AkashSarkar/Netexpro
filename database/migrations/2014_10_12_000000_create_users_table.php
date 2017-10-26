90f2054b7a1ec0bdfa337dfbfe2b2256416655cf
AkashSarkar <akashsarker1433@gmail.com>
2017-10-26 10:49:38 +0600

    env

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('education');
+            $table->string('profession');
+            $table->string('industry');
+
+            $table->string('')
             $table->rememberToken();
             $table->timestamps();
         });
